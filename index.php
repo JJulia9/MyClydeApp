d46fd4aae5689cf305835a67bd36b5084b09c80d
JJulia9 <juliaworkjj@gmail.com>
2023-12-06 11:19:32 +0000

    adding htaccess to change the url

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <?php
 include 'config/config.php';
-include 'partials/header.php'; 
+include 'partials/Header.php';
 ?>
 
-<!-- component -->
-<!-- Gogole Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;1,100;1,200&display=swap"
-    rel="stylesheet" />
 
-<style>
-    section {
-        font-family: "Poppins", sans-serif;
-    }
-</style>
 
 <!-- Page Main -->
 <main class="flex flex-col items-center justify-center mt-32">
@@ -257,78 +245,61 @@ include 'partials/header.php';
     </section>
 
     <!-- Content -->
-    <section class="p-20 space-y-8">
-        <h1 class="text-4xl text-center my-20">Tempor sit labore nostrud</h1>
+    <section class="bg-gray-50 dark:bg-white">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div class="max-w-screen-md mb-8 lg:mb-16">
+                <h2 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-grey">Designed for business teams like yours</h2>
+                <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            </div>
+            <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                <div>
+                    <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg class="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-grey">Marketing</h3>
+                    <p class="text-gray-500 dark:text-gray-400">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
+                </div>
+                <div>
+                    <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg class="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-grey">Legal</h3>
+                    <p class="text-gray-500 dark:text-gray-400">Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.</p>
+                </div>
+                <div>
+                    <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg class="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                    
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-grey">Business Automation</h3>
+                    <p class="text-gray-500 dark:text-gray-400">Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to help you get started.</p>
+                </div>
+                <div>
+                    <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg class="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-grey">Finance</h3>
+                    <p class="text-gray-500 dark:text-gray-400">Audit-proof software built for critical financial operations like month-end close and quarterly budgeting.</p>
+                </div>
+                <div>
+                    <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg class="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-grey">Enterprise Design</h3>
+                    <p class="text-gray-500 dark:text-gray-400">Craft beautiful, delightful experiences for both marketing and product with real cross-company collaboration.</p>
+                </div>
+                <div>
+                    <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg class="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-grey">Operations</h3>
+                    <p class="text-gray-500 dark:text-gray-400">Keep your company’s lights on with customizable, iterative, and structured workflows built for all efficient teams and individual.</p>
+                </div>
+            </div>
+        </div>
+      </section>
+      
 
-        <p>
-            Exercitation non Lorem exercitation tempor amet ad esse velit. Anim
-            deserunt id nulla quis ex magna culpa exercitation. Mollit consequat
-            enim eiusmod cupidatat commodo eiusmod nulla. Id quis amet incididunt
-            cillum dolore consectetur nulla incididunt laborum excepteur
-            consectetur consectetur sunt tempor. Minim fugiat sint sunt ad elit
-            laboris est sit est laboris labore do velit. Cillum aliquip pariatur
-            deserunt eiusmod. Consequat irure nulla adipisicing deserunt culpa.
-            Veniam enim deserunt sint consequat labore. Enim ullamco ea aute duis
-            et nostrud voluptate fugiat consectetur et velit cillum anim dolore.
-            In exercitation proident proident nostrud voluptate. Fugiat elit esse
-            labore pariatur ex. Ex labore esse voluptate nisi in. Ad anim deserunt
-            reprehenderit est. Aliquip pariatur nulla dolor amet quis aliqua sit
-            nisi officia enim velit. Ad enim eu dolor quis ipsum. Nulla amet
-            consectetur adipisicing ullamco qui tempor dolor excepteur dolore anim
-            pariatur quis laboris consequat. Consectetur ullamco consectetur minim
-            eiusmod quis veniam esse consectetur anim nostrud est pariatur
-            laborum. Mollit tempor minim amet esse. Lorem officia consequat
-            eiusmod sunt. Proident occaecat velit et consequat irure est cupidatat
-            tempor ullamco. Aliquip est pariatur ea ut qui ad sit dolor laboris
-            adipisicing officia eiusmod sit. Proident Lorem ex cillum sint
-            cupidatat amet nulla veniam laborum velit pariatur. Fugiat excepteur
-            culpa sunt est anim sunt. Ea voluptate non do proident sunt ad cillum
-            enim esse.
-        </p>
 
-        <p>
-            Incididunt eu pariatur laborum reprehenderit duis ex cillum fugiat.
-            Occaecat tempor commodo officia esse reprehenderit commodo. Irure nisi
-            culpa anim velit esse do esse veniam dolore fugiat. Non anim elit ad
-            ad esse veniam dolore labore consectetur nulla dolor occaecat quis
-            tempor. Ad do exercitation cillum duis consequat. Ea anim aute elit
-            velit duis quis. Anim sint ut occaecat officia id exercitation ea
-            incididunt exercitation sint ex. Reprehenderit ut qui eiusmod
-            cupidatat incididunt ad reprehenderit culpa tempor quis. Elit eu dolor
-            anim tempor eu fugiat voluptate dolor aliqua sint ex proident ullamco.
-            Labore deserunt velit quis cupidatat aliquip elit do labore ut.
-            Laborum dolore incididunt officia esse reprehenderit pariatur ullamco
-            consectetur tempor officia. Dolor culpa irure enim aute ex non veniam
-            eiusmod ipsum elit est sit mollit dolor. Est ad anim pariatur
-            consequat ad sit nisi.
-        </p>
-
-        <p>
-            Ipsum ea ad fugiat incididunt ea sit laborum cupidatat in ullamco
-            ullamco fugiat et id. Sit commodo enim enim veniam do. Ad id ad tempor
-            deserunt cupidatat reprehenderit ullamco officia excepteur culpa.
-            Officia amet in do voluptate exercitation pariatur officia et sit.
-            Adipisicing deserunt do aliquip ad id elit sint voluptate sit anim
-            nulla pariatur dolore. Lorem do commodo aute veniam deserunt magna
-            commodo cillum magna veniam. Ea amet nostrud commodo nisi adipisicing
-            cillum commodo. Nostrud aliquip cupidatat veniam culpa aute officia
-            commodo velit. Laborum quis aute nulla amet officia esse proident sunt
-            irure dolor fugiat incididunt occaecat. Commodo esse eiusmod fugiat eu
-            enim nisi sit id ut adipisicing. Enim nisi mollit ea aliqua nisi qui
-            ullamco eu velit et ullamco. Magna eiusmod eiusmod culpa proident
-            minim laboris fugiat enim laborum quis mollit minim do nisi. Elit
-            irure incididunt duis ullamco exercitation nulla fugiat. Eu ullamco
-            aliquip aute consectetur excepteur consequat. Non nostrud officia sint
-            enim et ad sunt amet pariatur deserunt enim reprehenderit. Aute Lorem
-            adipisicing laborum labore labore tempor officia officia reprehenderit
-            incididunt enim officia laborum. Proident eu irure occaecat ipsum elit
-            elit anim eu aute commodo ullamco reprehenderit incididunt elit.
-            Mollit voluptate eiusmod ullamco fugiat ex enim in fugiat et
-            consectetur duis do. Et aliqua ad labore labore ea ipsum nostrud
-            veniam et reprehenderit. Sint ad sit voluptate consequat cillum do
-            fugiat Lorem. Pariatur ad ad reprehenderit cupidatat nulla ipsum
-            voluptate. Dolore pariatur ea laboris est excepteur.
-        </p>
-    </section>
-</main>
-
+<?php
+    include 'partials/Footer.php';
+?>
